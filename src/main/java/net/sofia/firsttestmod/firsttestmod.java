a9458457a6f2d76610355396e87f1cfba035c44d
Sofia <robinsonsofia242@gmail.com>
2024-02-05 01:53:55 -0500

    Added test comment

--- src/main/java/net/sofia/firsttestmod/firsttestmod.java
+++ src/main/java/net/sofia/firsttestmod/firsttestmod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//test
+//test1
 public class firsttestmod implements ModInitializer {
 	public static final String MOD_ID = "firsttestmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
